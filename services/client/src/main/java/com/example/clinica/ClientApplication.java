d25e98b19af685a5df19249dc786e42afc54c60e
Maksimilian <maximka2002r@gmail.com>
2024-12-20 20:47:49 +0300

    re-init

--- services/client/src/main/java/com/example/clinica/ClientApplication.java
+++ services/client/src/main/java/com/example/clinica/ClientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PatientApplication {
+public class ClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PatientApplication.class, args);
+		SpringApplication.run(ClientApplication.class, args);
 	}
 
 }
